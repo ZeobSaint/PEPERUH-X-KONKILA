0d7d99a58bbd0b4e1a75a4c0f0571637296f9cb3
Jose Carlos Oliveira dos Santos Junior <jcosj1992@gmail.com>
2022-06-16 08:29:48 -0300

    16/06/2022
    - Build do FIND feita

--- Assets/Script Space/powerUpObt.cs
+++ Assets/Script Space/powerUpObt.cs
@@ -3,6 +3,33 @@ using UnityEngine;
 public class powerUpObt : MonoBehaviour
 {
     public AudioClip clipGet;
+    public Sprite[] spritePowers = new Sprite[3];
+    private playerInputs.powerUp power = playerInputs.powerUp.notHas;
+    private float duration = 0f;
+
+    private void OnEnable()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        renderer.sprite = spritePowers[2];
+
+        float aux = Random.value;
+
+        power = playerInputs.powerUp.confused;
+
+        if (aux > 2f / 3f)
+        {
+            renderer.sprite = spritePowers[1];
+            power = playerInputs.powerUp.shield;
+            duration = 12f;
+        }
+        else if (aux > 1f / 3f)
+        {
+            renderer.sprite = spritePowers[0];
+            power = playerInputs.powerUp.rapidFire;
+            duration = 4f;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -10,23 +37,6 @@ public class powerUpObt : MonoBehaviour
 
         if (player)
         {
-            float duration = 8f;
-
-            float aux = Random.value;
-
-            playerInputs.powerUp power = playerInputs.powerUp.confused;
-
-            if (aux > 2f / 3f)
-            {
-                power = playerInputs.powerUp.shield;
-                duration = 12f;
-            }
-            else if (aux > 1f / 3f)
-            {
-                power = playerInputs.powerUp.rapidFire;
-                duration = 4f;
-            }
-
             player.SetPowerUp(power, duration);
 
             gameObject.SetActive(false);
